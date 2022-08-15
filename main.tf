7d537cf697059baa5395cf623e71cc2f1d07ef61
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2022-08-15 10:44:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,3 +32,22 @@ variable "credentials" {
   })
   sensitive = true
 }
+  
+  
+output "region" {
+  value = module.aws_s3.s3_bucket_region
+}
+
+output "bucket" {
+  value = module.aws_s3.s3_bucket_bucket_domain_name
+}
+
+output "aws_access_key_id" {
+  value     = var.credentials.access_key
+  sensitive = true
+}
+
+output "aws_secret_access_key" {
+  value     = var.credentials.secret_key
+  sensitive = true
+}
